f6c77dd3db1af72f5659bf6f38425c791f96142e
wNSYA <ucokb037@gmail.com>
2023-11-02 13:47:45 +0700

    tes driver

--- src/ADT/Map/drivermap.c
+++ src/ADT/Map/drivermap.c
@@ -2,25 +2,37 @@
 #include "map.h"
 
 int main(){
-    Map Tesmap;
-    CreateEmpty(&Tesmap);
-    if (IsEmpty(Tesmap)){
-        printf("map kosong\n");
-    }
-    printf("melakukan pengisian map...\n");
-    for (int i = 0;i<MaxEl;i++){
-        keytype key = i;
-        valuetype value = i*i;
-        Insert(&Tesmap,key,value);
-    }
-    if (IsFull(Tesmap)){
-        printf("Map penuh.");
-    }
-    keytype key = 69;
-    printf("Key : %d, Value : %d\n",key, Value(Tesmap,key));
-    Delete(&Tesmap,key);
-    printf("Key : %d, Value : %d\n",key, Value(Tesmap,key));
-    if(!IsMember(Tesmap,key)){
-        printf("key %d tidak ditemukan\n", key);
-    }
+//     tes untuk value tipe integer
+//     untuk value adt(penyanyi, album dsb) menunggu adt nya kelar 
+//     Map Tesmap;
+//     CreateEmpty(&Tesmap);
+//     if (IsEmpty(Tesmap)){
+//         printf("map kosong\n");
+//     }
+//     printf("melakukan pengisian map...\n");
+//     for (int i = 0;i<MaxEl;i++){
+//         keytype key = i;
+//         valuetype value = i*i;
+//         Insert(&Tesmap,key,value);
+//     }
+//     if (IsFull(Tesmap)){
+//         printf("Map penuh.\n");
+//     }
+//     keytype key;
+//     printf("Masukkan key yang value nya ingin dicari : ");
+//     scanf("%d",&key);
+//     printf("Key : %d, Value : %d\n",key, Value(Tesmap,key));
+//     printf("Masukkan key yang ingin di delete : ");
+//     scanf("%d",&key); 
+//     printf("Key : %d, Value : %d\n",key, Value(Tesmap,key));
+//     printf("Melakukan penghapusan...\n");  
+//     Delete(&Tesmap,key);
+//     printf("Masukkan key yang value nya ingin dicari : ");
+//     scanf("%d",&key);
+//     if(!IsMember(Tesmap,key)){
+//         printf("key %d tidak ditemukan\n", key);
+//     }
+//     else{
+//         printf("Key : %d, Value : %d\n",key, Value(Tesmap,key));
+//     }
 }
