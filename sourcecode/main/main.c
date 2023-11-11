f4b78bc382ee18a757b86ebc419bdccfd49d00f1
wNSYA <ucokb037@gmail.com>
2023-11-11 15:56:16 +0700

    menambah help.h, menambah newline

--- sourcecode/main/main.c
+++ sourcecode/main/main.c
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 #include "../ADT/MesinKalimat/mesinkarakter.h"
 #include "../ADT/MesinKalimat/mesinkata.h"
 #include "ADT.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "../command/help.h"
 
 int main(){
     ListPenyanyi Penyanyi;
@@ -13,6 +13,7 @@ int main(){
     //MakeEmpty(&Penyanyi,&Album,&Song);
     printf(">>> ");
     char *start = "START;";
+    char *help = "HELP;";
     STARTINPUTKATA();
     if (IsEqual(currentWord,start)){
         printf("starting game..\n");
@@ -31,38 +32,7 @@ int main(){
             printf("    %d. %s\n",Song.Elements[i].IdAlbum,Song.Elements[i].namaS);
         }
     }
-=======
-#include "../ADT/MesinKalimat/mesinkarakter.h"
-#include "../ADT/MesinKalimat/mesinkata.h"
-#include "ADT.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-    ListPenyanyi Penyanyi;
-    MapAlbum Album;
-    SetSong Song;
-    //ID PasID;
-    //MakeEmpty(&Penyanyi,&Album,&Song);
-    printf(">>> ");
-    char *start = "START;";
-    STARTINPUTKATA();
-    if (IsEqual(currentWord,start)){
-        printf("starting game..\n");
-        char *file = "./sourcecode/config/config.txt";
-        STARTCOM(&Penyanyi,&Album,&Song, file);
-
-        for(int i = 0;i<Penyanyi.neff;i++){
-            printf("%d. %s\n",i+1,Penyanyi.A[i]);
-        }
-
-        for(int i = 0;i<Album.count;i++){
-            printf("  %d. %s\n",Album.Elements[i].IdPenyanyi,Album.Elements[i].valueAlbum);
-        }
-
-        for(int i = 0;i<Song.count;i++){
-            printf("    %d. %s\n",Song.Elements[i].IdAlbum,Song.Elements[i].namaS);
-        }
+    else if (IsEqual(currentWord,help)){
+        help_before();
     }
->>>>>>> c843b0ea9bdb4fc1c1ec1bef3605ea8942d5d361
 }
