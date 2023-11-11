acb70dbb8309e8df53ab27d9c9363455734e3309
Irfan Musthofa <im@irfanmusthofa.com>
2023-11-11 13:08:17 +0700

    Main.c udah final

--- main.c
+++ main.c
@@ -1,85 +1,33 @@
-#include "mesinkarakter.h"
-#include "mesinkata.h"
-#include "struct.h"
+#include "./src/mesinkarakter.h"
+#include "./src/mesinkata.h"
+#include "ADT.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main(){
-	/*KAMUS*/
-    List penyanyi;
-    Album album;
-    // SetSongs setsong;
-    char *file = "config.txt";
-    valuetype namaP, namaA, namaL;
-    int jumlahpenyanyi, jumlahalbum, jumlahlagu;
+    ListPenyanyi Penyanyi;
+    MapAlbum Album;
+    SetSong Song;
+    //ID PasID;
+    //MakeEmpty(&Penyanyi,&Album,&Song);
+    printf(">>> ");
+    char *start = "START;";
+    STARTINPUTKATA();
+    if (IsEqual(currentWord,start)){
+        printf("starting game..\n");
 
-    /*ALGORITMA*/
-    STARTWORD(file);
-    jumlahpenyanyi = WordToInt(currentWord);
-	penyanyi.nEff = jumlahpenyanyi;
+        STARTCOM(&Penyanyi,&Album,&Song);
 
-	/*TES PROGRAM*/
-    // printf("%d\n", penyanyi.nEff);
-	// printf("%s", );
-	// printf("tes\n")
+        for(int i = 0;i<Penyanyi.neff;i++){
+            printf("%d. %s\n",i+1,Penyanyi.A[i]);
+        }
 
-	printf("%d\n", penyanyi.nEff);
-	
-	for (int i = 0; i < jumlahpenyanyi; i++){
-		ADVWORD();
-		//ambil jumlah album dan simpan pada count album
-		Word takealbum = takeword(currentWord, 1);
-		jumlahalbum = WordToInt(takealbum);
-		penyanyi.contents[i].CountAlbum = jumlahalbum;
-		
-		//ambil nama penyanyi dan simpan pada ADT
-		Word takepenyanyi = takekata(currentWord);
-		namaP = WordToString(takepenyanyi);
-		penyanyi.contents[i].namapenyanyi = namaP;
-		printf("%s\n", penyanyi.contents[i].namapenyanyi);
-		//lakukan perulangan untuk album
-		for (int j = 0; j < jumlahalbum; j++){
-			ADVWORD();
-			//ambil jumlah lagu
-			Word takelagu = takeword(currentWord, 1);
-			jumlahlagu = WordToInt(takelagu);
-			album.CountSong = jumlahlagu;
-			// setsong.Count = jumlahlagu;
-			//ambil nama album 
-			Word takealbum = takekata(currentWord);
-			namaA = WordToString(takealbum);
-			penyanyi.contents[i].ElementsPenyanyi[j].namaalbum = namaA;
-			penyanyi.contents[i].ElementsPenyanyi[j].KeyAlbum = j + 1;
-			album.KeyAlbum =  j + 1;
-			printf("%d\n", jumlahlagu);
-			printf("%s\n", penyanyi.contents[i].ElementsPenyanyi[j].namaalbum);
-			//lakukan perulangan untuk lagu
-			
-			for (int k = 0; k < jumlahlagu; k++){
-				ADVWORD();
-				//ambil nama lagu
-				namaL = WordToString(currentWord);
-				// printf("%s\n", namaL);
-				album.ElementsAlbum[k].KeySong = k + 1;
-				album.ElementsAlbum[k].lagu = namaL;
-				// album.ElementsAlbum[k].lagu.nalbum = namaA;
-				// album.ElementsAlbum[k].lagu.npenyanyi = namaP;
-				printf("%s\n", album.ElementsAlbum[k].lagu);
+        for(int i = 0;i<Album.count;i++){
+            printf("  %d. %s\n",Album.Elements[i].IdPenyanyi,Album.Elements[i].valueAlbum);
+        }
 
-			}
-
-		}
-	}
-	
-	// printf("%s", penyanyi.contents[0].namapenyanyi);
-	// printf("halo");
-
+        for(int i = 0;i<Song.count;i++){
+            printf("    %d. %s\n",Song.Elements[i].IdAlbum,Song.Elements[i].namaS);
+        }
+    }
 }
-	// ADVWORD();
-	// 	Word takealbum = takeword(currentWord, 1);
-	// 	jumlahalbum = WordToInt(takealbum);
-	// 	Word takepenyanyi = takekata(currentWord);
-	// 	namaP = WordToString(takepenyanyi);
-	// 	penyanyi.contents[0].namapenyanyi = namaP;
-	// 	printf("%s", penyanyi.contents[0].namapenyanyi);
