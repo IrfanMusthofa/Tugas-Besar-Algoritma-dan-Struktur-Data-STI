25d67c904b4de98a2a9e42c399d6764814f1651a
Irfan Musthofa <im@irfanmusthofa.com>
2023-11-11 13:45:24 +0700

    typo

--- sourcecode/main/ADT.c
+++ sourcecode/main/ADT.c
@@ -26,7 +26,7 @@ void MakeEmpty(ListPenyanyi *Penyanyi,MapAlbum *Album,SetSong *Song){
 
 void STARTCOM(ListPenyanyi *Penyanyi,MapAlbum *Album,SetSong *Song){
     printf("init");
-    char *file = "./sourceode/config/config.txt";
+    char *file = "./sourcode/config/config.txt";
     STARTWORD(file);
     printf("ialize\n");
     int jlhPenyanyi=0;
