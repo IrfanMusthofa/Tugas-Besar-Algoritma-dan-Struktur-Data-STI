119d49926bd18cfcc4c54c65a76c3c8534c5ad5f
farahaulia115 <farahaulia115@gmail.com>
2023-11-18 09:18:44 +0700

    update command queue

---
+++ sourcecode/command/Queue/ADT.h
@@ -0,0 +1,115 @@
+#ifndef __adt__
+#define __adt__
+
+#include "boolean.h"
+#include "mesinkata.h"
+#include "mesinkarakter.h"
+
+#define NMAX 20
+#define EMPTY ""
+
+typedef int keytype;
+typedef char *valuetype;
+typedef char *infotype;
+typedef int IDs;
+typedef struct tElmtlist *address;
+
+
+//struct pada queue, stack dan playlist
+typedef struct{
+    valuetype namaP; 
+    valuetype namaA;
+    valuetype namaS;
+} Song;
+
+//playlist
+typedef struct tElmtlist { 
+	Song info;
+	address next;
+} ElmtList;
+
+typedef struct{
+    address First;
+	valuetype namaPlaylist;
+} List;
+
+typedef struct
+{
+    List *playlist;
+    int Capacity;
+    int Neff;
+} ArrayDinPlaylist;
+
+//stack
+typedef struct
+{
+    Song T[NMAX];
+    int TOP;
+} Stack;
+
+
+//queue
+typedef struct
+{
+    Song buffer [NMAX];
+    int idxTail;
+} queue;
+
+
+//struct pada setsong
+typedef struct{
+    IDs Idalbum;
+    valuetype namalagu;
+} elmtsong;
+
+typedef struct{
+    elmtsong Elements[NMAX*5];
+    int count;
+} SetSong;
+
+//struct pada album (20 kedikitan ey 60 lah ya)
+typedef struct{
+    keytype keyAlbum;
+    IDs IdPenyanyi;
+    valuetype valueAlbum;
+} Album;
+
+typedef struct{
+    Album Elements[NMAX*3];
+    int count;
+} MapAlbum;
+
+
+//struct liststatis penyanyi
+typedef char* Eltype;
+
+typedef struct{
+    Eltype A[NMAX];
+    int neff;
+} ListPenyanyi;
+
+
+//F.S nilai efektif ketiganya = 0
+void MakeEmpty(ListPenyanyi *Penyanyi,MapAlbum *Album,SetSong *Song);
+
+//Command START, read file default(config) dan memasukkan nilainya ke variable
+void STARTCOM(ListPenyanyi *Penyanyi,MapAlbum *Album,SetSong *Song, char *file);
+
+
+//I.S string penyanyi pastikan ada di list
+//F.S mengembalikan ID penyanyi dari string penyanyi input
+IDs GetPenyanyiID(ListPenyanyi List, char *penyanyi);
+
+//I.S string album pastikan ada di map
+//F.S mengembalikan ID album dari string album input
+IDs GetAlbumID (MapAlbum album, char *namaalbum);
+
+//I.S ID penyanyi pastikan ada
+//F.S mengembalikan jmlh album yang dimiliki penyanyi tsb
+int JumlahAlbum (MapAlbum album, IDs idpenyanyi);
+
+//I.S ID album pastikan ada
+//F.S mengembalikan jmlh lagu yang dimiliki album tsb
+int JumlahLagu (SetSong song, IDs idalbum);
+
+#endif
