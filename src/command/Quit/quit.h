c12af60003340e8a3832ae68277c8af4dde9a206
Irfan Musthofa <im@irfanmusthofa.com>
2023-11-19 01:09:56 +0700

    Ganti kapitall

---
+++ src/command/Quit/quit.h
@@ -0,0 +1,14 @@
+#ifndef __listdefaultadt__
+#define __listdefaultadt__
+
+#include "boolean.h"
+#include "mesinkata.h"
+#include "mesinkarakter.h"
+#include "ADT.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+void quit();
+
+#endif
