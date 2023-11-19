610bb025f0a862db515e1ac5c43c850c1bc05aab
farahaulia115 <farahaulia115@gmail.com>
2023-11-19 12:04:20 +0700

    update song

--- src/command/Song/song.c
+++ src/command/Song/song.c
@@ -1,35 +1,52 @@
 #include "song.h"
 
+
+/* Lanjut ke lagu berikutnya */
+/* IS : Currentsong, queue, stack terdefinisi */
+/* FS : Lagu yang paling depan di queue akan berada di current song, current song awal masuk ke stack */
 void songnext(CurrentSong *play, queue *currentqueue, Stack *previous){
-    Song currentlagu; Song nextlagu;
-    play->NamaA = currentlagu.namaA;
-    play->NamaP = currentlagu.namaP;
-    play->NamaS = currentlagu.namaS;
+    Song currentlagu; 
+    currentlagu.namaA = play->namaA;
+    currentlagu.namaP = play->NamaP;
+    currentlagu.namaS = play->NamaS;
+
+    Song nextlagu;
     dequeue(currentqueue, &nextlagu);
     play->NamaA = nextlagu.namaA;
     play->NamaP = nextlagu.namaP;
     play->NamaS = nextlagu.namaS;
+
     Push(previous, currentlagu);
+
     printf("Memutar lagu selanjutnya\n");
     printf("“%s” oleh “%s”\n", currentlagu.namaS, currentlagu.namaP);
 }
 
+/* Lanjut ke lagu sebelumnya */
+/* IS : Currentsong, queue, stack terdefinisi */
+/* FS : Lagu yang paling atas di stack, di pop, kemudian menjadi current song, current song masuk ke queue */
 void songprevious(CurrentSong *play, queue *currentqueue, Stack *previous){
     if (IsEmpty(*previous)){
         printf("Riwayat lagu kosong, memutar kembali lagu\n");
         printf("“%s” oleh “%s”\n", play->NamaS, play->NamaP);
     }
     else {
-        Song previoussong; Song songcurrent;
+        Song previoussong; 
+        Song songcurrent; // lagu yang dimainin sekarang, mau dibalikin lagi ke queue
         songcurrent.namaA = play->NamaA;
         songcurrent.namaP = play->NamaP;
         songcurrent.namaS = play->NamaS;
+        for (int i = currentqueue->idxTail; i >= 0; i--){
+            currentqueue->buffer[i+1] = currentqueue->buffer[i];
+        }
+        currentqueue->idxTail ++;
+        currentqueue->buffer[0] = songcurrent;
+
         Pop(previous, &previoussong);
-        Push(previous, songcurrent);
         play->NamaA = previoussong.namaA;
         play->NamaP = previoussong.namaP;
         play->NamaS = previoussong.namaS;
         printf("Memutar lagu sebelumnya\n");
-        printf("“%s” oleh “%s”\n", play->NamaS, play->NamaP);
+        printf("'%s' oleh '%s'\n", play->NamaS, play->NamaP);
     }
 }
--- src/command/Song/song.h
+++ src/command/Song/song.h
@@ -8,8 +8,14 @@
 #include "../Pick/pick.h"
 #include "../../ADT/Stack/stack.h"
 
+/* Lanjut ke lagu berikutnya */
+/* IS : Currentsong, queue, stack terdefinisi */
+/* FS : Lagu yang paling depan di queue akan berada di current song, current song awal masuk ke stack */
 void songnext(CurrentSong *play, queue *currentqueue, Stack *previous);
 
+/* Lanjut ke lagu sebelumnya */
+/* IS : Currentsong, queue, stack terdefinisi */
+/* FS : Lagu yang paling atas di stack, di pop, kemudian menjadi current song, current song masuk ke queue */
 void songprevious(CurrentSong *play, queue *currentqueue, Stack *previous);
 
 
